1a8a822d1c153f80e2651c36122bd8df12584e62
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-11-06 15:27:35 -0300

    Add project files.

---
+++ ExerciciosPropostos03/Program.cs
@@ -0,0 +1,17 @@
+﻿// See httusing System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d= int.Parse(Console.ReadLine());
+
+            int diferenca = a * b - c * d;
+
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+        }
+    }
+}
